1d6fcc0f78a83b42c9c598382b3e4782ebf25f1c
Priyanshu <Preyanshukandari@gmail.com>
2026-01-14 22:43:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import Left from "./Left"
+import Right from "./Right"
+
+const App = () => {
+
+  const[notes,setNotes] = useState(()=>{
+    return JSON.parse(localStorage.getItem("Notes")) || []
+  })
+
+  useEffect(()=>{
+    localStorage.setItem("Notes",JSON.stringify(notes))
+  },[notes])
+
+  const addNote = (note) =>{
+    setNotes([note,...notes])
+  }
+
+  const deleteNote = (id) => {
+  setNotes(prevNotes =>
+    prevNotes.filter(note => note.id !== id)
+  )
+}
+
+
+
+  return (
+    <div className="transition-all duration-200 ease-in-out flex lg:flex-row flex-col gap-15 min-h-screen lg:h-screen bg-gray-800 text-white lg:p-10 p-5">
+      <Left addNote={addNote}/>
+      <Right deleteNote={deleteNote} notes={notes} />
+    </div>
+  )
+}
+
+export default App
